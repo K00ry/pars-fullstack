86ca92cb1ec55b85a6a8de2c643c3aa81a4a34ae
K00ry <Koory.yellow@gmail.com>
2018-12-23 23:51:07 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+
+app.set("port", 5000);
+app.get("/", (req, res) => {
+    res.send("Welcome to Node + Express API");
+});
+const USERS = [
+    {
+        id: 0,
+        name: "Yomi Eluwande"
+    },
+    {
+        id: 1,
+        name: "Nate Murray"
+    },
+    {
+        id: 2,
+        name: "Sophia Shoemaker"
+    }
+];
+app.get("/users", (req, res) => {
+    res.send(USERS);
+});
+app.listen(app.get("port"), () => {
+    console.log("Node app is running on port", app.get("port"));
+});
