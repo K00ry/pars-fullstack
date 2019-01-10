82dd7139700eacfbd6680379f9d7cc9920eb508b
K00ry <Koory.yellow@gmail.com>
2019-01-09 23:01:21 -0500

    First product data imported and able to add new sub-genre to the model. YAAAAAY

--- models/kerbStone.js
+++ models/kerbStone.js
@@ -27,34 +27,13 @@ var ProductSchema = new Schema({
   sizes: [sizeSchema]
 });
 var KerbStone = mongoose.model("kerbstone", ProductSchema);
-// var sampleProduct = new KerbStone({
-//   genreEn: "jadval",
-//   genreFarsi: "جدول",
-//   genreId: "wet",
-//   type: "وت پرس",
-//   img: "/images/jadval/mobile/1.png",
-//   sizes: [
-//     {
-//       type: "۵۰*۳۰*۱۰",
-//       price: "۴۲۳۰۰",
-//       shipping: "۳۰۰۰"
-//     }
-//   ]
-// });
+
 // sampleProduct.save((err, product) => {
 //   if (err) console.log("Error", err);
 //   else console.log("product saved!");
 // });
-KerbStone.deleteMany({}, err => {
-  if (err) console.log("Error", err);
-});
+// KerbStone.deleteMany({}, err => {
+//   if (err) console.log("Error", err);
+// });
 
 module.exports.KerbStone = KerbStone;
-
-// var Dasti = new KerbStone({
-//   genreEn: "jadval",
-//   genreFarsi: "جدول",
-//   genreId: "wet",
-//   type: "وت پرس",
-//   img: "/images/jadval/mobile/1.png"
-// });
--- routes/kerbStone.js
+++ routes/kerbStone.js
@@ -6,10 +6,10 @@ router.get("/", (req, res, next) => {
   Product.find()
     .exec()
     .then(kerbs => {
-      const response = {
-        count: kerbs.length
-      };
-      res.status(200).json(response);
+      // const response = {
+      //   count: kerbs.length
+      // };
+      res.status(200).json(kerbs);
     })
     .catch(err => {
       console.log(err);
@@ -19,13 +19,30 @@ router.get("/", (req, res, next) => {
 
 router.post("/kerbStone", (req, res) => {
   console.log(req.body);
-  let jaber = new Product(req.body);
 
-  jaber.save((err, product) => {
-    if (err) console.log("Error", err);
-    else console.log("product saved!");
-    res.json(product);
-  });
+  Product.findOne({ genreId: req.body.genreId })
+    .exec()
+    .then(kerbs => {
+      kerbs.sizes.push({ kir: "kos" });
+      kerbs.save((err, prod) => {
+        console.log(err);
+
+        res.status(200).json(prod);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+
+  // Product.find()
+  // let jaber = new Product(req.body);
+  //
+  // jaber.save((err, product) => {
+  //   if (err) console.log("Error", err);
+  //   else console.log("product saved!");
+  //   res.json(product);
+  // });
 });
 
 module.exports = router;
