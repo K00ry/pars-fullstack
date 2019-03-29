e86200d85b64cd926f874ed340b476216e8d2112
Kourosh Mirzaei <koory@Kouroshs-iMac.fios-router.home>
2019-03-29 01:30:34 -0400

    added static file

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
 const mainRoutes = require("./routes");
+
 const KerbStoneroutes = require("./routes/kerbStone");
 
 const Blocks = require("./routes/blocks");
-const path = require("path");
+
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 
 const app = express();
 // const logger = require("morgan");
 //
 // app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.set("view engine", "pug");
+// app.use(cookieParser());
+// app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "client/build")));
 
 var mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/parsjadval");
-mongoose.connect(
-    "mongodb://koory:" +
-    process.env.MONGO_ATLAS_PW +
-    "@pars-jadval-shard-00-00-4kkmo.mongodb.net:27017,pars-jadval-shard-00-01-4kkmo.mongodb.net:27017,pars-jadval-shard-00-02-4kkmo.mongodb.net:27017/test?ssl=true&replicaSet=pars-jadval-shard-0&authSource=admin&retryWrites=true",
-    { useNewUrlParser: true }
-);
 
-// mongoose.connect(
-//   "mongodb+srv://koory:" +
-//     process.env.MONGO_ATLAS_PW +
-//     "@pars-jadval-4kkmo.mongodb.net/test?retryWrites=true"
-// );
+// mongoose.connect("process.env.MONGODB_URI");
+
+
+
+mongoose.connect(
+
+   process.env.MONGODB_URI
+    ||
+  "mongodb+srv://koory:" +
+    process.env.MONGO_ATLAS_PW +
+    "@pars-jadval-4kkmo.mongodb.net/test?retryWrites=true",
+     { useNewUrlParser: true }
+);
 
 const db = mongoose.connection;
 
@@ -57,6 +61,9 @@ app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
   res.render("error");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(process.env.PORT || 5000);
